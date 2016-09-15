d2c3addcf078995946b9aa1cb6101615bb20541b
TJ Hunter <tjhunter@gmail.com>
2016-09-15 12:02:50 -0600

    import board job

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,6 +2,7 @@
 
 namespace App\Console;
 
+use App\Console\Commands\GenerateImportBoardJobs;
 use App\Console\Commands\GenerateImportPinJobs;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;

6a5bed0b7d86d5ea2eea2b7d381e88d4a0787570
TJ Hunter <tjhunter@gmail.com>
2016-10-11 13:11:41 -0600

    use carbon

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use App\Board;
 use App\Policies\BoardPolicy;
+use Carbon\Carbon;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Gate;
 use Laravel\Passport\Passport;
